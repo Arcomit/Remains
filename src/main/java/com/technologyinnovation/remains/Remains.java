634db0dbb58722f90206519fc08222cf4c20449a
Moflop <8089341@qq.com>
2022-07-01 23:50:42 +0800

    Test

---
+++ src/main/java/com/technologyinnovation/remains/Remains.java
@@ -0,0 +1,15 @@
+package com.technologyinnovation.remains;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Remains implements ModInitializer {
+    public static final Logger LOGGER = LogManager.getLogger("firstmod");
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world!");
+
+    }
+}
